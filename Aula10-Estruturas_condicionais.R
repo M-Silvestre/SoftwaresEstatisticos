ce78935fd11beadcd8ed7e72617769bc1ece5747
M-Silvestre <43152458+M-Silvestre@users.noreply.github.com>
2019-07-26 14:24:44 -0300

    Update Aula10-Estruturas_condicionais.R

--- Aula10-Estruturas_condicionais.R
+++ Aula10-Estruturas_condicionais.R
@@ -1,4 +1,4 @@
-# IntroduÁ„o a softwares estatÌsticos
+# Introdu√ß√£o a softwares estat√≠sticos
 # Data: 17/07/2019
 # Aula: 10
 # Assunto: Estruturas condicionais
@@ -7,58 +7,58 @@
 # OBS: Operadores infixos ------------------------------------------
 
 # Em R existem os chamados "operadores infixos", cujos argumentos
-# n„o precisam estar contidos por ().
+# n√£o precisam estar contidos por ().
 
 2 + 3
 
-# Observe que todo operador infixo tambÈm pode ser chamado de
+# Observe que todo operador infixo tamb√©m pode ser chamado de
 # forma convencional
 '+'(2,3)
 
-# Exemplo: o operador %in% verifica se os elementos de um vetor tambÈm
+# Exemplo: o operador %in% verifica se os elementos de um vetor tamb√©m
 # ocorrem em outro. 
 c(1,2,7) %in% c(1,2,3,4,5)
-c("Pedro", "Rafael") %in% c("Rafael", "Jo„o", "JosÈ")
+c("Pedro", "Rafael") %in% c("Rafael", "Jo√£o", "Jos√©")
 
 
-# InteraÁ„o com o usu·rio --------------------------------------
+# Intera√ß√£o com o usu√°rio --------------------------------------
 
 
-# Assim como C, R tambÈm possui funÁıes para interagir com o usu·rio,
+# Assim como C, R tamb√©m possui fun√ß√µes para interagir com o usu√°rio,
 # como o print(), que exibe valores em tela.
 
 
 for(i in 1:10) 1:10
-# Todas as atribuiÁıes de valores a i ser„o feitas na memÛria, mas
-# nada ser· exibido
+# Todas as atribui√ß√µes de valores a i ser√£o feitas na mem√≥ria, mas
+# nada ser√° exibido
 
 for(i in 1:10) print(i)
-# Agora cada atribuiÁ„o ser· exibida.
+# Agora cada atribui√ß√£o ser√° exibida.
 
 # Exemplo:
 for(x in c(1,2,3)) print("String de caracteres")
 
 
-# OBS: n„o confundir exibiÁ„o na tela e retorno. Retorno È tudo que pode
-# ser utilizado (armazenado em uma vari·vel, passado como argumento
-# de funÁ„o, etc.)
+# OBS: n√£o confundir exibi√ß√£o na tela e retorno. Retorno √© tudo que pode
+# ser utilizado (armazenado em uma vari√°vel, passado como argumento
+# de fun√ß√£o, etc.)
 
 
-# A funÁ„o print() exibe apenas uma string ou o valor de um objeto.
-# J· cat() È uma funÁ„o mais vers·til, pois È possÌvel intercalar objetos
-# e strings, assim como exibir o valor de m˙ltiplos objetos.
+# A fun√ß√£o print() exibe apenas uma string ou o valor de um objeto.
+# J√° cat() √© uma fun√ß√£o mais vers√°til, pois √© poss√≠vel intercalar objetos
+# e strings, assim como exibir o valor de m√∫ltiplos objetos.
 
 nota <- 7
-cat("Jo„ozinho: Professor,\nqual a foi a minha nota?
-    Professor: Sua nota foi", nota, "\nJo„ozinho:\t...", sep = " ")
+cat("Jo√£ozinho: Professor,\nqual a foi a minha nota?
+    Professor: Sua nota foi", nota, "\nJo√£ozinho:\t...", sep = " ")
 
-# Para mais informaÁ„o, leia as documentaÁıes das funÁoes print() e cat():
+# Para mais informa√ß√£o, leia as documenta√ß√µes das fun√ßoes print() e cat():
 ?print
 ?cat
 
 
-# R tambÈm possui permite a inserÁ„o de valores pelo usu·rio
-# pela funÁ„o scan():
+# R tamb√©m possui permite a inser√ß√£o de valores pelo usu√°rio
+# pela fun√ß√£o scan():
 
 x <- scan(n = 3)
 
@@ -67,7 +67,7 @@ y <- scan(what = character())
 cat(y)
 
 
-# Estruturas condionais ------------------------------------------------
+# Estruturas condicionais ------------------------------------------------
 
 # O R possui estrutura condicional, o if(). Funciona semelhantemente
 # como em C.
@@ -75,24 +75,24 @@ x <- 7
 z <- x
 
 if(x > 0){
-  cat('x È positivo.')
+  cat('x √© positivo.')
   y <- x/z
 } else{
-  cat('x n„o È positivo')
+  cat('x n√£o √© positivo')
   y <- z
 }
 y
 
-# OBS: A boa identaÁ„o (recuo) de um cÛdigo deixa-o mais legÌvel por
-# ressaltar a sua estrutura. Por exemplo, no cÛdigo acima a identaÁ„o
-# deixa vÌsivel quais linhas de comando pertencem ao bloco if e quais
+# OBS: A boa identa√ß√£o (recuo) de um c√≥digo deixa-o mais leg√≠vel por
+# ressaltar a sua estrutura. Por exemplo, no c√≥digo acima a identa√ß√£o
+# deixa v√≠sivel quais linhas de comando pertencem ao bloco if e quais
 # pertencem ao bloco else.
 
 # O atalho Ctrl + Shift + A serve para identar automaticamente um
-# bloco de cÛdigo.
+# bloco de c√≥digo selecionado.
 
 
-# TambÈm È possÌvel aninhar estruturas if():
+# Tamb√©m √© poss√≠vel aninhar estruturas if():
 
 idade <- 30
 if(idade < 18){
@@ -104,56 +104,61 @@ if(idade < 18){
 } else{
   grupo <- 4
 }
+    
 grupo
 
-# OBS: chamamos de bloco de instruÁ„o o cÛdigo contido em um {}.
-# Apesar do cÛdigo acima ter apenas uma linha por bloco, um bloco
-# pode conter v·rias linhas de cÛdigo e, assim como em C, podemos
-# omitir o {} o bloco È apenas uma linha. R tambÈm permite
-# que todo o comando if else esteja contido em uma ˙nica linha
+# OBS: chamamos de bloco de instru√ß√£o o c√≥digo contido em um {}.
+# Apesar do c√≥digo acima ter apenas uma linha por bloco, um bloco
+# pode conter v√°rias linhas de c√≥digo e, assim como em C, podemos
+# omitir o {} o bloco √© apenas uma linha. R tamb√©m permite
+# que todo o comando if() else esteja contido em uma √∫nica linha
 
 nota <- 7
 if(nota > 7) cat("Aluno> =(") else cat("Aluno: =)")
 
 
-# A funÁ„o ifelse È um comando if() else resumido.
-# Ela recebe trÍs argumentos: a condiÁ„o, a express„o a ser
-# retornada caso a condiÁ„o seja atendida, e o retorno caso
-# a condiÁ„o n„o seja atendida.
+# A fun√ß√£o ifelse() √© um comando if() else resumido.
+# Ela recebe tr√™s argumentos: a condi√ß√£o, a express√£o a ser
+# retornada caso a condi√ß√£o seja atendida, e o retorno caso
+# a condi√ß√£o n√£o seja atendida.
 
-set.seed(2) # Fixamos a semente usada na geraÁ„o de n˙meros aleatÛrios
+set.seed(2) # Fixamos a semente usada na gera√ß√£o de n√∫meros aleat√≥rios
+          
 x <- rnorm(n = 5, mean = 0, sd = 1)
 sig <- ifelse(x < 0, "-", "+")
 sig
 
 
-# O comando set.seed() serve para fixar um conjunto de n˙meros pseudo
-# aleatÛrios a serem gerados. Isso permite que os resultados de
+# O comando set.seed() serve para fixar um conjunto de n√∫meros pseudo
+# aleat√≥rios a serem gerados. Isso permite que os resultados de
 # um teste possam ser reproduzidos com o mesmo resultado.
 
 
-# Uma outra instruÁ„o instruÁ„o condicional que poder· ser utilizada
-# para escolher uma de v·rias alternativas possÌveis È a instruÁ„o switch().
-# Ela consiste em uma sÈrie de argumentos que a depender de uma condiÁ„o
-# (primeiro argumento) que ser„o ou n„o executados.
+# Uma outra instru√ß√£o instru√ß√£o condicional que poder√° ser utilizada
+# para escolher uma de v√°rias alternativas poss√≠veis √© a instru√ß√£o switch().
+# Ela consiste em uma s√©rie de argumentos que a depender de uma condi√ß√£o
+# (primeiro argumento) que ser√£o ou n√£o executados.
 
 set.seed(0)
+          
 expressao <- 1
+          
 vetor_normal <- rnorm(10)
+          
 switch (EXPR = expressao,
         round(mean(vetor_normal),1),
         round(median(vetor_normal))
 )
 
-# Quando o nome dos cases n„o È especificado, por padr„o o R os
-# nomeia usando o n˙mero do case. No cÛdigo acima, o primeiro
-# caso È a mÈdia dos n˙meros em vetor_normal arredondada para a primeira
-# casa decimal e o segundo È a mediana, tambÈm arredondada.
+# Quando o nome dos cases n√£o √© especificado, por padr√£o o R os
+# nomeia usando o n√∫mero do case. No c√≥digo acima, o primeiro
+# caso √© a m√©dia dos n√∫meros em vetor_normal arredondada para a primeira
+# casa decimal e o segundo √© a mediana, tamb√©m arredondada.
 
 
-# OBS: se o case fornecido n„o existe, o comando switch() n„o resulta
+# OBS: se o case fornecido n√£o existe, o comando switch() n√£o resulta
 # em erros, mas retorna NULL. Deve-se ter isso em mente quando se
-# atribui o retorno do switch() a uma vari·vel.
+# atribui o retorno do switch() a uma vari√°vel.
 
 
 # Exemplo: escreva um programa que calcule o imposto pago por mulheres
@@ -167,10 +172,10 @@ salario <- 5e03
 
 if(sexo == "M" || sexo == "m"){ 
   imposto <- salario*0.15
-  cat("O imposto a pagar È: ", imposto)
+  cat("O imposto a pagar √©: ", imposto)
 } else { 
   imposto <- salario*0.10
-  cat("O imposto a pagar È:", imposto)
+  cat("O imposto a pagar √©:", imposto)
 }
 
 
@@ -180,9 +185,9 @@ imposto <- switch (EXPR = sexo,
         "M" = salario*0.15,
         "F" = salario*0.1
 )
-cat("O imposto a pagar È:", imposto)
+cat("O imposto a pagar √©:", imposto)
 
 # Usando o ifelse()
 
 imposto <- ifelse(sexo == "M"|| sexo == "m", salario*0.15, salario*0.1)
-cat("O imposto a pagar È:", imposto)
+cat("O imposto a pagar √©:", imposto)
